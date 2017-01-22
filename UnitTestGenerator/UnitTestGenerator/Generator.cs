33cd00bc47d418a1dcf3ca26556e00e43c4887c5
Vasyl Bukshovan <vasyl.bukshovan@winks.io>
2017-01-22 12:58:08 +0200

    first commit

---
+++ UnitTestGenerator/UnitTestGenerator/Generator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestGenerator
+{
+    public class Generator<T>
+    {
+
+        public string GenerateFromFile(string jsonFile)
+        {
+
+            if (!System.IO.File.Exists(jsonFile))
+            {
+                throw new Exception(string.Format("Error : File {0} not found!", jsonFile));
+            }
+            string json = System.IO.File.ReadAllText(jsonFile, Encoding.UTF8);
+
+            return Generate(json);
+        }
+        public string Generate(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                throw new Exception(string.Format("Error : json is null or empty"));
+            }
+            Type type = typeof(T);
+
+            return null;
+        }
+    }
+}
---
+++ UnitTestGenerator/UnitTestGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string jsonFile = ConfigurationManager.AppSettings["jsonFileLocation"];
+            Console.WriteLine("Json Source File : {0}", jsonFile);
+
+            Generator<BeProduct.Core.DataModel.Folder.Folder> generator = new Generator<BeProduct.Core.DataModel.Folder.Folder>();
+            generator.GenerateFromFile(jsonFile);
+
+            Console.WriteLine("\r\n\tpress Q to exit ...");
+            while (Console.ReadKey().Key != ConsoleKey.Q) ;
+       }
+    }
+}
